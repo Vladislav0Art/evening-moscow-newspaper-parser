99b84dbd5509a225ed107a6657892cbf36f7a9d4
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2024-11-07 08:43:41 +0100

    feat: write script to migrate urls from csv into json file

---
+++ insert.py
@@ -0,0 +1,19 @@
+import csv
+import json
+
+with open('input.csv', 'r') as input, open('input.json') as file:
+    reader = csv.reader(input)
+    rows = list(reader)[1:]
+
+    config = json.load(file)
+
+    if "urls" not in config:
+        config["urls"] = []
+
+    for row in rows:
+        url, year, date = row
+        print("Read the row:", row)
+        config["urls"].append(url)
+
+    with open('input.json', 'w') as output:
+        json.dump(config, output, indent=4)
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import logging
 import os
 import csv
 import json
-import sys
 import argparse
 
 
